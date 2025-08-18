40bb0992a89059085df0f2c8058f31b170742ae3
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-18 10:41:37 -0300

    :sparkles: feature: Add Persistence Implementation with PatientRepositoryImpl

---
+++ src/main/java/br/com/nicolasfrech/HealthTrack/infra/patient/gateway/PatientRepositoryImpl.java
@@ -0,0 +1,25 @@
+package br.com.nicolasfrech.HealthTrack.infra.patient.gateway;
+
+import br.com.nicolasfrech.HealthTrack.application.patient.gateway.PatientRepository;
+import br.com.nicolasfrech.HealthTrack.domain.Patient;
+import br.com.nicolasfrech.HealthTrack.infra.patient.persistence.PatientEntity;
+import br.com.nicolasfrech.HealthTrack.infra.patient.persistence.PatientRepositoryJPA;
+
+public class PatientRepositoryImpl implements PatientRepository {
+
+    private final PatientEntityMapper mapper;
+    private final PatientRepositoryJPA jpaRepository;
+
+    public PatientRepositoryImpl(PatientEntityMapper mapper, PatientRepositoryJPA jpaRepository) {
+        this.mapper = mapper;
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Override
+    public Patient save(Patient patient) {
+        PatientEntity entity = mapper.toEntity(patient);
+
+        jpaRepository.save(entity);
+        return mapper.toDomain(entity);
+    }
+}
