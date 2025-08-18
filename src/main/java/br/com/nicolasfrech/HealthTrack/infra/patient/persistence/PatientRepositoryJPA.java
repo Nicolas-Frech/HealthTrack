91b3841cfd2e639f6d1afb1d036e77714954be26
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-18 10:29:27 -0300

    :sparkles: feature: Create PatientRepositoryJPA interface

---
+++ src/main/java/br/com/nicolasfrech/HealthTrack/infra/patient/persistence/PatientRepositoryJPA.java
@@ -0,0 +1,6 @@
+package br.com.nicolasfrech.HealthTrack.infra.patient.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepositoryJPA extends JpaRepository<PatientEntity, Long> {
+}
