c5075e2454bfc9545a10674aadf9602606865a5f
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-18 10:01:34 -0300

    :wrench: conf: Add SecurityConfiguration

---
+++ src/main/java/br/com/nicolasfrech/HealthTrack/infra/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.nicolasfrech.HealthTrack.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/patient").permitAll();
+                })
+                .build();
+    }
+}
