05eeb0eea3ef17fcd57aad5dd74cc8e46199aa3c
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-18 16:59:45 -0300

    :sparkles: feature: add PatientValidator for Domain Class

--- src/main/java/br/com/nicolasfrech/HealthTrack/domain/patient/Patient.java
+++ src/main/java/br/com/nicolasfrech/HealthTrack/domain/patient/Patient.java
@@ -15,46 +15,32 @@ public class Patient {
     public Patient() {
     }
 
+    public Patient(String name, String cpf, Integer age, String email, String telephone) {
+        this.name = name;
+        this.cpf = PatientValidator.validateCPF(cpf);
+        this.age = age;
+        this.email = PatientValidator.validateEmail(email);
+        this.telephone = PatientValidator.validateTelephone(telephone);
+        this.active = true;
+    }
+
     public Patient(Long id, String telephone, String name, String cpf, Integer age, String email, Boolean active) {
         this.id = id;
-        this.telephone = telephone;
+        this.telephone = PatientValidator.validateTelephone(telephone);
         this.name = name;
-        this.cpf = cpf;
+        this.cpf = PatientValidator.validateCPF(cpf);
         this.age = age;
-        this.email = email;
+        this.email = PatientValidator.validateEmail(email);
         this.active = active;
     }
 
-    public Patient(String name, String cpf, Integer age, String email, String telephone) {
-        this.name = name;
-        if(!cpf.matches("^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$|^\\d{11}$")) {
-            throw new IllegalArgumentException("CPF INVÁLIDO!");
-        }
-
-        this.cpf = cpf;
-        this.age = age;
-
-        if(!email.matches("^[\\w.-]+@[\\w.-]+\\.\\w{2,}$")) {
-            throw new IllegalArgumentException("E-MAIL INVÁLIDO!");
-        }
-
-        this.email = email;
-
-        if(!telephone.matches("^\\(\\d{2}\\)\\s?\\d{4,5}-\\d{4}$")) {
-            throw new IllegalArgumentException("TELEFONE INVÁLIDO!");
-        }
-
-        this.telephone = telephone;
-        active = true;
-    }
-
     public void deletePatient() {
-        active = false;
+        this.active = false;
     }
 
     public void updatePatient(PatientUpdateDTO dto) {
         if(dto.email() != null && !dto.email().isBlank()) {
-            this.email = dto.email();
+            this.email = PatientValidator.validateEmail(dto.email());
         }
         if(dto.age() != null && dto.age() > 0) {
             this.age = dto.age();
---
+++ src/main/java/br/com/nicolasfrech/HealthTrack/domain/patient/PatientValidator.java
@@ -0,0 +1,24 @@
+package br.com.nicolasfrech.HealthTrack.domain.patient;
+
+public class PatientValidator {
+    public static String validateCPF(String cpf) {
+        if (cpf == null || !cpf.matches("^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$|^\\d{11}$")) {
+            throw new IllegalArgumentException("CPF INVÁLIDO!");
+        }
+        return cpf;
+    }
+
+    public static String validateEmail(String email) {
+        if (email == null || !email.matches("^[\\w.-]+@[\\w.-]+\\.\\w{2,}$")) {
+            throw new IllegalArgumentException("E-MAIL INVÁLIDO!");
+        }
+        return email;
+    }
+
+    public static String validateTelephone(String telephone) {
+        if (telephone == null || !telephone.matches("^\\(\\d{2}\\)\\s?\\d{4,5}-\\d{4}$")) {
+            throw new IllegalArgumentException("TELEFONE INVÁLIDO!");
+        }
+        return telephone;
+    }
+}
