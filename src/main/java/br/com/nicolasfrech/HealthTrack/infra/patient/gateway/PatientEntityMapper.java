61fea927059338bf8f76489770b31e219357605e
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-18 10:37:35 -0300

    :sparkles: feature: Create PatientEntityMapper

---
+++ src/main/java/br/com/nicolasfrech/HealthTrack/infra/patient/gateway/PatientEntityMapper.java
@@ -0,0 +1,24 @@
+package br.com.nicolasfrech.HealthTrack.infra.patient.gateway;
+
+import br.com.nicolasfrech.HealthTrack.domain.Patient;
+import br.com.nicolasfrech.HealthTrack.infra.patient.persistence.PatientEntity;
+
+public class PatientEntityMapper {
+
+    public PatientEntity toEntity(Patient patient) {
+        return new PatientEntity(patient.getTelephone(),
+                patient.getEmail(),
+                patient.getAge(),
+                patient.getCpf(),
+                patient.getName(),
+                patient.getId());
+    }
+
+    public Patient toDomain(PatientEntity entity) {
+        return new Patient(entity.getId(), entity.getTelephone(),
+                entity.getName(),
+                entity.getCpf(),
+                entity.getAge(),
+                entity.getEmail());
+    }
+}
