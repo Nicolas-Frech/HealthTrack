68ffe8ab4c4d5cc1871e5353324450812a867e3d
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-20 19:36:13 -0300

    :recycle: refactor: Change IllegalArgumentException to ValidateException

--- src/main/java/br/com/nicolasfrech/HealthTrack/domain/validation/DomainValidator.java
+++ src/main/java/br/com/nicolasfrech/HealthTrack/domain/validation/DomainValidator.java
@@ -1,30 +1,32 @@
 package br.com.nicolasfrech.HealthTrack.domain.validation;
 
+import br.com.nicolasfrech.HealthTrack.infra.exception.ValidateException;
+
 public class DomainValidator {
     public static String validateCPF(String cpf) {
         if (cpf == null || !cpf.matches("^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$|^\\d{11}$")) {
-            throw new IllegalArgumentException("CPF INVÁLIDO!");
+            throw new ValidateException("CPF INVÁLIDO!");
         }
         return cpf;
     }
 
     public static String validateEmail(String email) {
         if (email == null || !email.matches("^[\\w.-]+@[\\w.-]+\\.\\w{2,}$")) {
-            throw new IllegalArgumentException("E-MAIL INVÁLIDO!");
+            throw new ValidateException("E-MAIL INVÁLIDO!");
         }
         return email;
     }
 
     public static String validateTelephone(String telephone) {
         if (telephone == null || !telephone.matches("^\\(\\d{2}\\)\\s?\\d{4,5}-\\d{4}$")) {
-            throw new IllegalArgumentException("TELEFONE INVÁLIDO!");
+            throw new ValidateException("TELEFONE INVÁLIDO!");
         }
         return telephone;
     }
 
     public static String validateCRM(String crm) {
         if (crm == null || !crm.matches("^[A-Z]{2}\\d{4,6}$")) {
-            throw new IllegalArgumentException("CRM INVÁLIDO!");
+            throw new ValidateException("CRM INVÁLIDO!");
         }
         return crm;
     }
