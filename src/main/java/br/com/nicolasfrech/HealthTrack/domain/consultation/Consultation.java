02265f0f750a9a654111be47cef97dc156e94679
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-21 17:14:01 -0300

    :sparkles: feature: Create Consultation Domain class

---
+++ src/main/java/br/com/nicolasfrech/HealthTrack/domain/consultation/Consultation.java
@@ -0,0 +1,62 @@
+package br.com.nicolasfrech.HealthTrack.domain.consultation;
+
+import java.time.LocalDateTime;
+
+public class Consultation {
+
+    private Long id;
+    private Long medicId;
+    private Long patientId;
+    private LocalDateTime date;
+    private String notes;
+    private String prescription;
+    private ConsultationStatus status;
+
+    public Consultation() {
+    }
+
+    public Consultation(Long id, Long medicId, Long patientId, LocalDateTime date, String notes, String prescription, ConsultationStatus status) {
+        this.id = id;
+        this.medicId = medicId;
+        this.patientId = patientId;
+        this.date = date;
+        this.notes = notes;
+        this.prescription = prescription;
+        this.status = status;
+    }
+
+    public Consultation(Long medicId, Long patientId, LocalDateTime date) {
+        this.medicId = medicId;
+        this.patientId = patientId;
+        this.date = date;
+        this.status = ConsultationStatus.SCHEDULED;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMedicId() {
+        return medicId;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public ConsultationStatus getStatus() {
+        return status;
+    }
+}
---
+++ src/main/java/br/com/nicolasfrech/HealthTrack/domain/consultation/ConsultationStatus.java
@@ -0,0 +1,8 @@
+package br.com.nicolasfrech.HealthTrack.domain.consultation;
+
+public enum ConsultationStatus {
+    SCHEDULED,
+    COMPLETED,
+    CANCELED,
+    NO_SHOW
+}
