15d8df70d3b500e1cfe2fb16c606d4edaf1b2230
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-22 10:59:17 -0300

    :white_check_mark: test: Create ValidateAdvanceNoticeTest class

---
+++ src/test/java/br/com/nicolasfrech/HealthTrack/application/consultation/ValidateAdvanceNoticeTest.java
@@ -0,0 +1,40 @@
+package br.com.nicolasfrech.HealthTrack.application.consultation;
+
+import br.com.nicolasfrech.HealthTrack.application.consultation.dto.BookConsultationDTO;
+import br.com.nicolasfrech.HealthTrack.application.consultation.validation.bookConsultation.ValidateAdvanceNotice;
+import br.com.nicolasfrech.HealthTrack.infra.exception.ValidateException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class ValidateAdvanceNoticeTest {
+
+    @Autowired
+    private ValidateAdvanceNotice validateAdvanceNotice;
+
+    @Test
+    @DisplayName("Should book consultation with 1 hour advance notice")
+    public void validateAdvanceNoticeScenario01() {
+        BookConsultationDTO dto = new BookConsultationDTO("SC123456", "123.456.789-00",
+                LocalDateTime.now().plusDays(1).withHour(14).withMinute(30));
+
+        assertDoesNotThrow(() -> validateAdvanceNotice.validate(dto));
+    }
+
+    @Test
+    @DisplayName("Should NOT book consultation without 1 hour advance notice")
+    public void validateAdvanceNoticeScenario02() {
+        BookConsultationDTO dto = new BookConsultationDTO("SC123456", "123.456.789-00",
+                LocalDateTime.now());
+
+        assertThrows(ValidateException.class, () -> validateAdvanceNotice.validate(dto));
+    }
+
+}
