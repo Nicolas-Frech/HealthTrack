89219db88b0db3e484eaf9aac646c53d6c67b4cc
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-22 08:10:14 -0300

    :sparkles: feature: Create ValidateAdvanceNotice class

--- src/main/java/br/com/nicolasfrech/HealthTrack/application/consultation/ConsultationService.java
+++ src/main/java/br/com/nicolasfrech/HealthTrack/application/consultation/ConsultationService.java
@@ -2,12 +2,16 @@ package br.com.nicolasfrech.HealthTrack.application.consultation;
 
 import br.com.nicolasfrech.HealthTrack.application.consultation.dto.BookConsultationDTO;
 import br.com.nicolasfrech.HealthTrack.application.consultation.gateway.ConsultationRepository;
+import br.com.nicolasfrech.HealthTrack.application.consultation.validation.BookConsultationValidation;
 import br.com.nicolasfrech.HealthTrack.application.medic.gateway.MedicRepository;
 import br.com.nicolasfrech.HealthTrack.application.patient.gateway.PatientRepository;
 import br.com.nicolasfrech.HealthTrack.domain.consultation.Consultation;
 import br.com.nicolasfrech.HealthTrack.domain.medic.Medic;
 import br.com.nicolasfrech.HealthTrack.domain.patient.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ConsultationService {
@@ -16,6 +20,9 @@ public class ConsultationService {
     private final MedicRepository medicRepository;
     private final PatientRepository patientRepository;
 
+    @Autowired
+    private List<BookConsultationValidation> bookConsultationValidations;
+
     public ConsultationService(ConsultationRepository consultationRepository, MedicRepository medicRepository, PatientRepository patientRepository) {
         this.consultationRepository = consultationRepository;
         this.medicRepository = medicRepository;
@@ -23,6 +30,8 @@ public class ConsultationService {
     }
 
     public Consultation bookConsultation(BookConsultationDTO dto) {
+        bookConsultationValidations.forEach(v -> v.validate(dto));
+
         Medic medic = medicRepository.findByCrmAndActiveTrue(dto.medicCRM());
         Patient patient = patientRepository.findByCpfAndActiveTrue(dto.patientCPF());
 
---
+++ src/main/java/br/com/nicolasfrech/HealthTrack/application/consultation/validation/ValidateAdvanceNotice.java
@@ -0,0 +1,23 @@
+package br.com.nicolasfrech.HealthTrack.application.consultation.validation;
+
+import br.com.nicolasfrech.HealthTrack.application.consultation.dto.BookConsultationDTO;
+import br.com.nicolasfrech.HealthTrack.infra.exception.ValidateException;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ValidateAdvanceNotice implements BookConsultationValidation {
+
+    @Override
+    public void validate(BookConsultationDTO dto) {
+        LocalDateTime consultationDate = dto.date();
+        LocalDateTime now = LocalDateTime.now();
+        var diferenceBetween = Duration.between(consultationDate, now).toHours();
+
+        if(diferenceBetween < 60) {
+            throw new ValidateException("Consulta deve ser agendada com no mínimo 1 hora de antecedência!");
+        }
+    }
+}
