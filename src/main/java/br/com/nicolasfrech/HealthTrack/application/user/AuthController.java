20fff0567ea97302852a5dfbd28c74df901c4d6c
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-26 09:42:31 -0300

    :sparkles: feature: Finalize AuthController registUser method

--- src/main/java/br/com/nicolasfrech/HealthTrack/application/user/AuthController.java
+++ src/main/java/br/com/nicolasfrech/HealthTrack/application/user/AuthController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,10 +22,13 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @PostMapping
+    @PostMapping("/register")
     @Transactional
-    public ResponseEntity<UserReturnDTO> registUser(@RequestBody @Valid UserRegistDTO dto) {
+    public ResponseEntity<UserReturnDTO> registUser(@RequestBody @Valid UserRegistDTO dto, UriComponentsBuilder uriBuilder) {
         User user = authService.registUser(dto);
-        return null;
+        URI uri = uriBuilder.path("/auth/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new UserReturnDTO(user));
+
     }
 }
