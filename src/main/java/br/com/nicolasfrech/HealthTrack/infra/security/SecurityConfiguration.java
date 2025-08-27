365394f8a4e44d7195d5a27693ff333745cc382b
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-27 10:34:27 -0300

    :sparkles: feature: Adjust SecurityConfiguration for ADMIN and MEDIC roles

--- src/main/java/br/com/nicolasfrech/HealthTrack/infra/security/SecurityConfiguration.java
+++ src/main/java/br/com/nicolasfrech/HealthTrack/infra/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.nicolasfrech.HealthTrack.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,8 +28,20 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> req
-                        .anyRequest().permitAll()
-                )
+                        .requestMatchers("/auth/**").permitAll()
+
+                        // ADMIN
+                        .requestMatchers(HttpMethod.POST, "/consultation").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/consultation/*/status").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/consultation/*/date").hasRole("ADMIN")
+                        .requestMatchers("/patient/**", "/medic/**").hasRole("ADMIN")
+
+                        // MEDIC
+                        .requestMatchers(HttpMethod.PUT, "/consultation/*/notes").hasRole("MEDIC")
+                        .requestMatchers(HttpMethod.GET, "/consultation/medic/*").hasRole("MEDIC")
+
+
+                        .requestMatchers(HttpMethod.GET, "/consultation").hasAnyRole("ADMIN", "MEDIC")).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
