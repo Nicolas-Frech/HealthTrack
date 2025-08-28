a07733ae20f50e0c48fc8a553db209b75583d5df
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-08-28 08:53:52 -0300

    :sparkles: feature: Implement Creation of User MEDIC when a Medic is register

--- src/main/java/br/com/nicolasfrech/HealthTrack/application/medic/MedicService.java
+++ src/main/java/br/com/nicolasfrech/HealthTrack/application/medic/MedicService.java
@@ -4,10 +4,14 @@ import br.com.nicolasfrech.HealthTrack.application.medic.dto.MedicRegistDTO;
 import br.com.nicolasfrech.HealthTrack.application.medic.dto.MedicUpdateDTO;
 import br.com.nicolasfrech.HealthTrack.application.medic.gateway.MedicRepository;
 import br.com.nicolasfrech.HealthTrack.application.medic.validation.MedicRegistValidation;
+import br.com.nicolasfrech.HealthTrack.application.user.gateway.UserRepository;
 import br.com.nicolasfrech.HealthTrack.domain.medic.Medic;
+import br.com.nicolasfrech.HealthTrack.domain.user.Role;
+import br.com.nicolasfrech.HealthTrack.domain.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,12 +20,17 @@ import java.util.List;
 public class MedicService {
 
     private final MedicRepository medicRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     private List<MedicRegistValidation> registerValidations;
 
-    public MedicService(MedicRepository medicRepository) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public MedicService(MedicRepository medicRepository, UserRepository userRepository) {
         this.medicRepository = medicRepository;
+        this.userRepository = userRepository;
     }
 
     public Medic registMedic(MedicRegistDTO dto) {
@@ -30,7 +39,12 @@ public class MedicService {
         Medic medic = new Medic(dto.name(), dto.crm(), dto.speciality(), dto.telephone(),
                 dto.email());
 
+        String medicPassword = dto.crm().substring(0, 3) + dto.telephone().replaceAll("\\D", "").substring(0, 5);
+
+        User user = new User(medic.getCrm(), passwordEncoder.encode(medicPassword), Role.MEDIC);
+
         medicRepository.save(medic);
+        userRepository.save(user);
         return medic;
     }
 
